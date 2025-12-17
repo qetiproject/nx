e72ab6c3e41359135d5dd538bbb29192ae291401
qetiproject <ketideveloper@gmail.com>
2025-12-17 21:50:34 +0400

    initial, add app, add app with react

--- apps/shop-e2e/eslint.config.mjs
+++ apps/shop-e2e/eslint.config.mjs
@@ -1,9 +1,9 @@
-import baseConfig from '../eslint.base.config.mjs';
 import cypress from 'eslint-plugin-cypress/flat';
+import baseConfig from '../../eslint.base.config.mjs';
 
 export default [
-  ...baseConfig,
   cypress.configs['recommended'],
+  ...baseConfig,
   {
     // Override or add rules here
     rules: {},
