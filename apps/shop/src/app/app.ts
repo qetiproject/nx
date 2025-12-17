d5d5276b57f52d23667c32f8123796be507267f3
qetiproject <ketideveloper@gmail.com>
2025-12-17 22:05:16 +0400

    shared libs

--- apps/shop/src/app/app.ts
+++ apps/shop/src/app/app.ts
@@ -1,5 +1,6 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 import { RouterModule } from '@angular/router';
+import { range } from '@foocaompany/utils';
 import { NxWelcome } from './nx-welcome';
 
 @Component({
@@ -8,6 +9,11 @@ import { NxWelcome } from './nx-welcome';
   templateUrl: './app.html',
   styleUrl: './app.css',
 })
-export class App {
+export class App implements OnInit{
+
   protected title = 'shop';
+
+  ngOnInit(): void {
+    console.log(range(40, 45))
+  }
 }
