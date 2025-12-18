4ec673e31b84279f1c0181a797b17e11ba978c1d
qetiproject <ketideveloper@gmail.com>
2025-12-18 22:43:10 +0400

    auth feature login

---
+++ auth/src/lib/feature-login/feature-login.ts
@@ -0,0 +1,9 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'lib-feature-login',
+  imports: [],
+  templateUrl: './feature-login.html',
+  styleUrl: './feature-login.css',
+})
+export class FeatureLogin {}
